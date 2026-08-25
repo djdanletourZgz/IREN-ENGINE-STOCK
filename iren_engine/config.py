435e954f83ef0639d62218723a826f01a6149009
djdanletourZgz <djdanletour@gmail.com>
2026-08-25 09:52:09 +0200

    Add V2-A AI universe configuration

--- iren_engine/config.py
+++ iren_engine/config.py
@@ -7,6 +7,8 @@ class EngineConfig:
     qqq: str = "QQQ"
     nvda: str = "NVDA"
     vix: str = "^VIX"
+    eurusd: str = "EURUSD=X"
+    ai_basket: tuple[str, ...] = ("SOXX", "NVDA", "MU", "MRVL", "AVGO", "AMD", "VRT", "CRWV", "NBIS")
     daily_period: str = "5y"
     intraday_period: str = "60d"
     intraday_interval: str = "5m"
@@ -14,6 +16,8 @@ class EngineConfig:
     intraday_neighbors: int = 160
     min_daily_samples: int = 50
     min_intraday_samples: int = 60
+    v2_lookback_rows: int = 756
+    v2_recency_half_life: int = 252
     options_max_dte: int = 45
 
 CFG = EngineConfig()
